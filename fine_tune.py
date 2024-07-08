db2d861ed9366663c37bb92e9bbc8fa7a85d053f
Ridhwan bakare <ridmartrite@gmail.com>
2024-07-08 14:38:43 +0100

    adding fine tune file

---
+++ fine_tune.py
@@ -0,0 +1,14 @@
+import torch
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArguments, TextDataset, DataCollatorForLanguageModeling
+
+model_name = 'gpt2'
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+
+def load_dataset(file_path, tokenizer, block_size=128):
+    dataset = TextDataset(
+        tokenizer=tokenizer,
+        file_path=file_path,
+        block_size=block_size
+    )
+    return dataset
